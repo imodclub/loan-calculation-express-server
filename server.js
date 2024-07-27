31e4a0cd8883f68703567ba54ba369c5cc8a4394
imodclub <ikamol.me@gmail.com>
2024-07-27 21:37:47 +0700

    change and add app.use bodyParser and Path

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 // server.js
 const express = require('express');
 const bodyParser = require('body-parser');
-const cors = require('cors');
+const path = require('path');
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors());
 app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'public')));
 
 
 // server.js
